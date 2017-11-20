2fc9796ae0dc6f2991b6b2a5c84904214236f577
prista <games.prista@mail.ru>
2017-11-20 13:59:47 +0300

    DailyAdvice maven project with log4j

---
+++ actual/lrn/DailyAdvice/src/main/java/dailyadvice/DailyAdviceClient.java
@@ -0,0 +1,27 @@
+package dailyadvice;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class DailyAdviceClient {
+    public void go() {
+        try {
+            Socket s = new Socket("127.0.0.1", 8080);
+            InputStreamReader streamReader = new InputStreamReader(s.getInputStream());
+            BufferedReader reader = new BufferedReader(streamReader);
+
+            String advice = reader.readLine();
+            System.out.println("Сегодня ты должен: " + advice);
+
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new DailyAdviceClient().go();
+    }
+}
---
+++ actual/lrn/DailyAdvice/src/main/java/dailyadvice/DailyAdviceServer.java
@@ -0,0 +1,47 @@
+package dailyadvice;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import org.apache.log4j.Logger;
+
+public class DailyAdviceServer {
+    private static final Logger log = Logger.getLogger(DailyAdviceServer.class);
+
+
+    String[] adviceList = {"Ешьте меньшими порциями", "Купите облегающие джинсы. Нет, они не делают вас полнее", "Два слова: не годится",
+    "Будьте честны хотябы сегодня, скажите своему боссу все, что вы о нем думаете", "Возможно, вам стоит подобрать другую прическу"};
+
+    public void go() {
+        log.info("Это информационное сообщение!");
+        try {
+            ServerSocket serverSocket = new ServerSocket(8080);
+            log.info("Создан serverSocket!");
+            while (true) {
+                log.info("Ждем подключение");
+                Socket socket = serverSocket.accept();
+                log.info("Клиент подключился! " + socket.getInetAddress() + " port: " + socket.getPort());
+                PrintWriter writer = new PrintWriter(socket.getOutputStream());
+                String advice = getAdvice();
+                writer.println(advice);
+                writer.close();
+                log.info(advice);
+                System.out.println(advice);
+            }
+        } catch (IOException ex) {
+            log.error(ex.getMessage());
+        }
+    }
+
+    private String getAdvice() {
+        int random = (int) (Math.random() * adviceList.length);
+        log.info("Номер совета: " + adviceList[random]);
+        return adviceList[random];
+
+    }
+
+    public static void main(String[] args) {
+        new DailyAdviceServer().go();
+    }
+}
